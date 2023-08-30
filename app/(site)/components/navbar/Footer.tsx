e7314bb8e5353685f1992b3d1f0de9286e9c9133
des98 <dev.desertcod98@proton.me>
2023-08-30 17:29:18 +0200

    text-ellipsis in footer

--- app/(site)/components/navbar/Footer.tsx
+++ app/(site)/components/navbar/Footer.tsx
@@ -1,17 +1,24 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import { redirect } from "next/navigation";
-import FooterAvatar from "./FooterAvatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
 
 export default async function Footer() {
   const user = await getCurrentUser();
 
-  if(!user){
+  if (!user) {
     redirect("/login");
   }
 
   return (
     <div className="w-full h-24 border-t-2 flex items-center px-3">
-      <FooterAvatar image={user.image} fallback={user.name[0]}/>
+      <Link href={"/profile"} className="flex w-2/5 gap-5 items-center">
+        <Avatar className="cursor-pointer hover:opacity-80">
+          <AvatarImage src={user.image ?? undefined} />
+          <AvatarFallback>{user.name[0]}</AvatarFallback>
+        </Avatar>
+        <span className="text-ellipsis whitespace-nowrap overflow-hidden">{user.name}</span>
+      </Link>
     </div>
   );
 }

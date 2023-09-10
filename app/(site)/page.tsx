1b28412d7232d4b8c5d3da0d2d03e00fb22ee73b
des98 <dev.desertcod98@proton.me>
2023-09-10 12:08:24 +0200

    changed home

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,14 +1,12 @@
-"use client";
-
-import { signOut } from "next-auth/react";
+import { BsChatLeftText } from "react-icons/bs";
 
 export default async function Home() {
-
   return (
-    <div className="h-full">
-Home
-    <button onClick={() => signOut()}>logout</button>
+    <div className="h-full w-full flex items-center justify-center">
+      <div className="flex flex-col items-center rounded-full bg-slate-100 w-96 h-96 justify-center gap-3">
+        <BsChatLeftText size={150} opacity={0.4} />
+        <span className="font-medium text-center">Select an existing chat <br/>or create a new one</span>
+      </div>
     </div>
-
   );
 }
